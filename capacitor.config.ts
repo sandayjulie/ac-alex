e346e3af40b0e28d6b99c700e1bc161f93d1c2a3
julie <85165965+sandayjulie@users.noreply.github.com>
2023-09-12 12:32:09 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CEP-AC',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
